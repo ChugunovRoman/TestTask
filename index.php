d6d960691ba591bffc1f0e5af4525078617da1da
ChugunovRoman <Zebs-BMK@yandex.ru>
2016-09-27 12:19:46 +0300

    add output array in the logger

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ $loader = require_once __DIR__.'/vendor/autoload.php'; // автозагрузк
 
 $config = Config::getInstance();
 
+$arr = ['key1' => "name", 
+        'key2' => "value"];
+
 $fileLogger = new LoggerFactory($config->getConfig());
 $fileLogger = $fileLogger->logger();
-$fileLogger->log("log text");
+$fileLogger->log($arr);
--- scripts/Logger.php
+++ scripts/Logger.php
@@ -6,6 +6,12 @@ abstract class Logger
     
     public function log($entry)
     {
+        if(is_array($entry) or is_object($entry)) {
+            foreach($entry as $key => $value) {
+                $some_text .= ' ['.$key.'] => '.$value;
+            }
+            $entry = $some_text;
+        }
         $this->text = date("[Y-m-d H:i:s] ").$entry;
         $this->write();
     }
